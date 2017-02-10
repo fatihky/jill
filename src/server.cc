8f0bce2689764f5afa5b65950bb304be1ec7b84c
Fatih Kaya <1994274@gmail.com>
2017-02-10 22:37:28 +0300

    create sample table with timestamp field

--- src/server.cc
+++ src/server.cc
@@ -1,5 +1,12 @@
 #include "table.hh"
 
+using namespace jill::table;
+
 int main(int argc, char *argv[]) {
+	jill::table::Table table;
+	jill::table::Field<jill::table::TIMESTAMP, int64_t> *timestamp = new jill::table::Field<jill::table::TIMESTAMP, int64_t>("timestamp");
+
+	table.addField<TIMESTAMP, int64_t>(timestamp);
+
 	return 0;
 }
